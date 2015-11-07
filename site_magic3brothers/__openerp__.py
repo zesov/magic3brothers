363a1375383c1f22ac4badf2da88613705c7194a
Jodoo <63720750@qq.com>
2015-11-08 03:15:38 +0800

    init

---
+++ site_magic3brothers/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# author: 63720750@qq.com
+{
+    'name': 'Magic3brothers Theme',
+    'description': 'This module provides feature for changing layout of theme.',
+    'summary': 'This module provides feature for changing layout of theme.',
+    'category': 'Theme',
+    'version': '1.0',
+    'website': 'http://www.yunaio.com',
+    'author': 'Jack.cao',
+    'depends': ['website','website_blog'],
+    'data': [
+        'views/theme.xml',
+    ],
+    'images':[
+            'static/description/splash_screen.png',
+    ],
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
