d95ee4a0deb43da2a405d9812b5a76dbef0f68e2
zesov <baich.info@gmail.com>
2015-11-08 04:03:02 +0800

    Update __openerp__.py

--- site_magic3brothers/__openerp__.py
+++ site_magic3brothers/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # author: 63720750@qq.com
 {
-    'name': 'Magic3brothers Theme',
+    'name': 'Carousel-thumbnail theme',
     'description': 'This module provides feature for carousel-thumbnail-indicators theme.',
     'summary': 'This module provides feature for carousel-thumbnail-indicators theme.',
     'category': 'Theme',
