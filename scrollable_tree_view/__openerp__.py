cf67b0536520a6fe17cc49d47982a82ee98e23b8
Jodoo <63720750@qq.com>
2015-11-09 00:29:18 +0800

    scrollable tree view

---
+++ scrollable_tree_view/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# author: baich.info@gmail.com
+{
+    'name': 'Tree view scrollable',
+    'description': 'This module provides feature for Odoo tree view horizontal scrollable.',
+    'summary': 'This module provides feature for Odoo tree view horizontal scrollable.',
+    'category': 'Tools',
+    'version': '1.0',
+    'website': 'http://www.yunaio.com',
+    'author': 'Jodoo',
+    'depends': ['base'],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'images':[
+            'static/description/main_screenshot.jpg',
+    ],
+    'application': False,
+    'price': 9.99,
+    'currency': 'EUR',
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
