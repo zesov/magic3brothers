e4dc034231832e46861f00c5eb3f504b83e5781f
Administrator <ccj@newbeemall.com>
2016-04-21 23:01:15 +0800

    author

--- site_magic3brothers/__openerp__.py
+++ site_magic3brothers/__openerp__.py
@@ -1,14 +1,20 @@
+# -*- coding: utf-8 -*-
+# author: baich.info@gmail.com
 {
     'name': 'Magic3brothers Theme',
-    'summary': 'Support for Customize themes in 9.0',
-    'description': 'This theme module is support for customize themes in website 9.0',
+    'description': 'This module provides feature for changing layout of theme.',
+    'summary': 'This module provides feature for changing layout of theme.',
     'category': 'Theme',
-    'sequence': 900,
     'version': '1.0',
+    'website': 'http://www.yunaio.com',
+    'author': 'Jack.cao',
     'depends': ['website','theme_default'],
     'data': [
         'views/theme.xml',
     ],
-    'images': ['static/description/icon.png'],
-    'application': False,
+    'images':[
+            'static/description/splash_screen.png',
+    ],
+    'application': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
