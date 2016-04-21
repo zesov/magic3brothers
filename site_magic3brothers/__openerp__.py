deb44c7b7a05e18ee4119a1750837c5fa4ceeaac
zesov <baich.info@gmail.com>
2016-04-21 23:38:38 +0800

    Update __openerp__.py

--- site_magic3brothers/__openerp__.py
+++ site_magic3brothers/__openerp__.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
-# author: baich.info@gmail.com
+# author: 63720750@qq.com
 {
-    'name': 'Magic3brothers Theme',
-    'description': 'This module provides feature for changing layout of theme.',
-    'summary': 'This module provides feature for changing layout of theme.',
+    'name': 'Carousel thumbnail Theme',
+    'description': 'This module provides feature for carousel-thumbnail-indicators theme.',
+    'summary': 'This module provides feature for carousel-thumbnail-indicators theme.',
     'category': 'Theme',
     'version': '1.0',
     'website': 'http://www.yunaio.com',
-    'author': 'Jack.cao',
+    'author': 'Jodoo',
     'depends': ['website','theme_default'],
     'data': [
         'views/theme.xml',
     ],
     'images':[
-            'static/description/splash_screen.png',
+            'static/description/main_screenshot.jpg',
     ],
     'application': True,
+    'live_test_url': 'http://demo.aioyun.com',
+    # 'price': 9.99,
+    # 'currency': 'EUR',
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
